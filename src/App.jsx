ac64e37b1a7ebcf49ed424986db39dd23fa0ce62
alexdgiworx <alex@dgiworx.in>
2025-06-02 19:03:36 +0530

    updated app2

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>ALEX HACKS</h1>
+      <h1>ARULPANDI</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
